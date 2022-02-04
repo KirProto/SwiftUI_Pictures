bb4eb9fbd4e110b98d003972f0ec4d99db1ecd71
Kir Proto Q <kir.proto@ya.ru>
2022-02-04 07:08:37 +0500

    Initial Commit

---
+++ Yan_SwiftUI_Pictures/Yan_SwiftUI_PicturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_PicturesApp.swift
+//  Yan_SwiftUI_Pictures
+//
+//  Created by KIR Q on 2022.02.04.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_PicturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
